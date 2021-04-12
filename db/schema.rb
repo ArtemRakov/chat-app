0c434a93765f8ad44fa7d6983fa4d9dc198fee06
Artem Rakov <scoff1k@gmail.com>
2021-04-12 02:21:07 +0000

    setup vim with solargraph

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,79 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2021_04_12_020601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "team_channel_members", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "channel_id", null: false
+    t.bigint "team_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["channel_id"], name: "index_team_channel_members_on_channel_id"
+    t.index ["team_id"], name: "index_team_channel_members_on_team_id"
+    t.index ["user_id"], name: "index_team_channel_members_on_user_id"
+  end
+
+  create_table "team_channel_messages", force: :cascade do |t|
+    t.bigint "channel_id", null: false
+    t.bigint "team_id", null: false
+    t.bigint "user_id", null: false
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["channel_id"], name: "index_team_channel_messages_on_channel_id"
+    t.index ["team_id"], name: "index_team_channel_messages_on_team_id"
+    t.index ["user_id"], name: "index_team_channel_messages_on_user_id"
+  end
+
+  create_table "team_channels", force: :cascade do |t|
+    t.string "name"
+    t.string "slug"
+    t.bigint "team_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["slug"], name: "index_team_channels_on_slug", unique: true
+    t.index ["team_id"], name: "index_team_channels_on_team_id"
+  end
+
+  create_table "team_members", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "team_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["team_id"], name: "index_team_members_on_team_id"
+    t.index ["user_id"], name: "index_team_members_on_user_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "owner_id", null: false
+    t.string "slug"
+    t.index ["owner_id"], name: "index_teams_on_owner_id"
+    t.index ["slug"], name: "index_teams_on_slug", unique: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "team_channel_members", "team_channels", column: "channel_id"
+  add_foreign_key "team_channel_members", "teams"
+  add_foreign_key "team_channel_members", "users"
+  add_foreign_key "team_channel_messages", "team_channels", column: "channel_id"
+  add_foreign_key "team_channel_messages", "teams"
+  add_foreign_key "team_channel_messages", "users"
+  add_foreign_key "team_channels", "teams"
+  add_foreign_key "team_members", "teams"
+  add_foreign_key "team_members", "users"
 end
